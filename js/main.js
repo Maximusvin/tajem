3f87ffdd6b8d4e80ba353f0729e0d6ff7ae80004
Maxim Kozlov <Maximusvin@gmail.com>
2021-02-11 00:57:59 +0200

    create header

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+  $('body').on('click', '[href*="#"]', function (e) {
+    var fixed_offset = '';
+    $('html,body')
+      .stop()
+      .animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 400);
+    e.preventDefault();
+  });
+});
+
+$(function () {
+  $('.header__burger').on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+    $('.burger-wrap').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+});
