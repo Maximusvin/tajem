9e21122244d1d96c41285b88162bba7e22629917
Maxim Kozlov <Maximusvin@gmail.com>
2021-02-12 23:57:03 +0200

    added form contact

--- js/slider.js
+++ js/slider.js
@@ -1,26 +1,26 @@
-const prev = document.getElementById("btn-prev");
-const next = document.getElementById("btn-next");
-const slides = document.querySelectorAll(".slide");
-const dots = document.querySelectorAll(".dot");
+const prev = document.getElementById('btn-prev');
+const next = document.getElementById('btn-next');
+const slides = document.querySelectorAll('.slide');
+const quote = document.querySelectorAll('.quote');
 
 let index = 0;
 
 const activeClass = (idx, nods) => {
   for (nod of nods) {
-    nod.classList.remove("active");
+    nod.classList.remove('active');
   }
-  nods[idx].classList.add("active");
+  nods[idx].classList.add('active');
 };
 
 const nextSlide = () => {
   if (index === slides.length - 1) {
     index = 0;
     activeClass(index, slides);
-    activeClass(index, dots);
+    activeClass(index, quote);
   } else {
     index++;
     activeClass(index, slides);
-    activeClass(index, dots);
+    activeClass(index, quote);
   }
 };
 
@@ -28,19 +28,19 @@ const prevSlide = () => {
   if (index === 0) {
     index = slides.length - 1;
     activeClass(index, slides);
-    activeClass(index, dots);
+    activeClass(index, quote);
   } else {
     index--;
     activeClass(index, slides);
-    activeClass(index, dots);
+    activeClass(index, quote);
   }
 };
 
-dots.forEach((item, indexDot) => {
-  item.addEventListener("click", () => {
+quote.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
     index = indexDot;
     activeClass(index, slides);
-    activeClass(index, dots);
+    activeClass(index, quote);
   });
 });
 
@@ -50,7 +50,7 @@ const changeSlide = () => {
   }, 2000);
 };
 
-next.addEventListener("click", nextSlide);
-prev.addEventListener("click", prevSlide);
+next.addEventListener('click', nextSlide);
+prev.addEventListener('click', prevSlide);
 
 changeSlide();
