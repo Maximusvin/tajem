9d5fa4c11a23bf9fd18bd011739adbe5e0bb9282
Maxim Kozlov <Maximusvin@gmail.com>
2021-02-13 17:41:58 +0200

    added form and clients

---
+++ i.js
@@ -0,0 +1,13 @@
+console.log(2 + '3');
+console.log('3' + 1);
+
+console.log(null + '2' + 6);
+console.log(undefined + 'e' + 2);
+
+const a = [1, 2, 3];
+const b = a.splice();
+
+b.push(4);
+
+console.log(a);
+console.log(b);
